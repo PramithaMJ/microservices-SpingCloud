96bea040366a09f21f8a4dc0e50293b099b249bc
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-12-18 21:28:31 +0530

    V1.2

--- limit-service/src/main/java/com/pmj/limmit/service/controller/LimitsController.java
+++ limit-service/src/main/java/com/pmj/limmit/service/controller/LimitsController.java
@@ -1,14 +1,20 @@
 package com.pmj.limmit.service.controller;
 
 import com.pmj.limmit.service.bean.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pmj.limmit.service.configuration.Configuration;
+
+
 @RestController
 public class LimitsController {
+    @Autowired
+    private Configuration configuration;
 
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-        return new Limits(1,100);
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
     }
 }
